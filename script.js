4c5fd767a30cc12dd1be54e8559c3c31e5c812d1
bminczu <bminczu@gmail.com>
2021-07-07 13:44:15 -0500

    selection button fxn ok. selections WIP

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const selectionButtons = document.querySelectorAll('[data-selection]')
+const SELECTIONS = [
+  {
+    name: 'rock',
+    emoji: '✊',
+    beats: 'scissors'
+  },
+  
+  {
+    name: 'paper',
+    emoji: '✋',
+    beats: 'rock',
+  },
 
+  {
+    name: 'scissors',
+    emoji: '✌',
+    beats: 'paper'
+  }
+]
 
 
 selectionButtons.forEach(selectionButton => {
